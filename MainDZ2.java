75546a0f082acf3cc1e8ccf95ef1e085be509d72
maksim <your>
2023-12-12 16:15:34 +1100

    MainDZ2.java

---
+++ MainDZ2.java
@@ -0,0 +1,25 @@
+
+// Напишите свой код в методе printPrimeNums класса Answer.
+// Пример
+// 1 // 2 // 3 // 5 // 7 // 11 // ...
+// 1е решение задачи
+
+class Answer { public void printPrimeNums(){
+
+    int min = 1;
+    int max = 1001;
+    Answer random = new Answer();
+    for (int i = 1; i < 1001; i++) {
+
+    System.out.println(i);
+    }
+}
+}
+
+class Printer{ public static void main(String[] args) {
+
+    Answer ans = new Answer();
+    ans.printPrimeNums();
+}
+}    
+
