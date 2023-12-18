c4b936b72fffc3db24e476b9b0e4da31aaad423d
maksim <your>
2023-12-19 10:35:45 +1100

    Main3_Sem_4

---
+++ Main3_Sem_4.java
@@ -0,0 +1,37 @@
+// 1) Написать метод, который принимает массив элементов, помещает их в стэк
+// и выводит на консоль содержимое стэка.
+// 2) Написать метод, который принимает массив элементов, помещает их в
+// очередь и выводит на консоль содержимое очереди. 
+import java.util.*;
+
+
+public class Main3_Sem_4 {
+    public static void main(String[] args) {
+        Integer[] arr = {1,2,3,4,5};
+        printAsQueue(arr);
+    }
+
+    static void printAsQueue(Integer[] arr){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.addAll(Arrays.asList(arr));
+
+        while (!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+    }
+
+    static void printAsStack(Integer[] arr){
+        Stack<Integer> stack = new Stack<>();
+        stack.addAll(Arrays.asList(arr));
+
+//        System.out.println(stack);
+
+//        for (int a: stack){
+//            System.out.println(a);
+//        }
+
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
+    }
+}
