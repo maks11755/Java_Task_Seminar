80acebc029720a88c9c82e4aeddafe5eb005e1a2
maksim <your>
2023-12-19 15:24:17 +1100

    Main_Sem_6

---
+++ Main_Sem_6.java
@@ -0,0 +1,23 @@
+// 1. Создайте HashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
+// Распечатайте содержимое данного множества.
+// 2. Создайте LinkedHashSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5,
+// 6, 3}. Распечатайте содержимое данного множества.
+// 3. Создайте TreeSet, заполните его следующими числами: {1, 2, 3, 2, 4, 5, 6, 3}.
+// Распечатайте содержимое данного множества.
+
+import java.util.*;
+
+
+
+public class Main_Sem_6{
+    public static void main(String[] args) {
+        Integer[] arr = {4, 5, 1, 2, 3, 2, 6, 3, 99};
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(arr));
+        Set<Integer> set2 = new LinkedHashSet<>(Arrays.asList(arr));
+        Set<Integer> set3 = new TreeSet<>(Arrays.asList(arr));
+
+        System.out.println(set1);
+        System.out.println(set2);
+        System.out.println(set3);
+    }
+}
